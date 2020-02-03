868af888ff01dcbc42c2422fdca5f025e4aeade9
mdmustafanaim121 <mdmustafanaim121@gmail.com>
2020-02-03 10:27:48 -0600

    Decider functionality is now working

--- app/src/main/java/com/mufti/decidex/MainActivity.kt
+++ app/src/main/java/com/mufti/decidex/MainActivity.kt
@@ -2,11 +2,27 @@ package com.mufti.decidex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val array = ArrayList<String>();
+
+        addButton.setOnClickListener() {
+            val item = textValue.text.toString()
+            array.add(item)
+        }
+
+        decide.setOnClickListener(){
+            val random = Random()
+            val randomItem = random.nextInt(array.count())
+            textDisplay.text = array[randomItem]
+        }
     }
 }
